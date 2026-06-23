81e15bb663db3fbdf93006ac8f7870cebdbb2f94
Your Name <your@email.com>
2026-06-23 13:02:06 +0300

    commit all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,5 +15,12 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
