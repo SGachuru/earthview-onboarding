78b6e6fa995c459b7bcbbaf00f12be521a81b9be
Your Name <your@email.com>
2026-06-30 13:50:53 +0300

    commit all

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,9 @@
-const path = require('path');
-const express = require('express');
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 const dotenv = require('dotenv');
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
-  });
-}
 
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
